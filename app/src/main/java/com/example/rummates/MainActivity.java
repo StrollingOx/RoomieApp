3bffce913eecdb81dac20d548cfb71079ab368ac
Adven97 <atomczak30@gmail.com>
2019-11-04 21:48:38 +0100

    add login functionality, without validation

--- app/src/main/java/com/example/rummates/MainActivity.java
+++ app/src/main/java/com/example/rummates/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.rummates.signIn.SignIn;
 import com.facebook.AccessToken;
 import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
@@ -33,9 +35,10 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MainActivity extends AppCompatActivity {
 
     private LoginButton loginButton;
-    private Button magicButton, registerButton;
+    private Button magicButton, registerButton, signInButton;
     private CircleImageView circleImageView;
     private TextView textName, textEmail;
+    String nick,password;
 
     private CallbackManager callbackManager;
 
@@ -46,10 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
         loginButton = findViewById(R.id.login_button);
         magicButton = findViewById(R.id.magic_button);
+
         registerButton = findViewById(R.id.register_button);
-        textEmail = findViewById(R.id.profile_email);
-        textName = findViewById(R.id.profile_name);
-        circleImageView = findViewById(R.id.profile_pic);
+        signInButton = findViewById(R.id.sign_in_button);
+//        textEmail = findViewById(R.id.profile_email);
+//        textName = findViewById(R.id.profile_name);
+//        circleImageView = findViewById(R.id.profile_pic);
+
+
 
         callbackManager = CallbackManager.Factory.create();
         loginButton.setPermissions(Arrays.asList("email","public_profile"));
@@ -81,6 +88,25 @@ public class MainActivity extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                EditText nickField = (EditText) findViewById(R.id.sign_in_username);
+                EditText passwordField = (EditText) findViewById(R.id.sign_in_password);
+
+                nick = nickField.getText().toString();
+                password = passwordField.getText().toString();
+
+                SignIn signIn = new SignIn(nick, password);
+                String status = signIn.handleSignIn(nick,password);
+                Toast.makeText(getBaseContext(), status, Toast.LENGTH_LONG).show();
+                if(Integer.parseInt(status) == 201){
+                    //TODO go to Michal's user page
+                }
             }
         });
     }
---
+++ app/src/main/java/com/example/rummates/signIn/SignIn.java
@@ -0,0 +1,147 @@
+package com.example.rummates.signIn;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class SignIn {
+
+
+    private static String nick, password;
+    private static final String BASE_URL = "https://rumies.herokuapp.com";
+    private static final String LOGIN = "/users/login";
+
+
+    public SignIn(String nick, String password){
+        this.nick = nick;
+        this.password = password;
+    }
+
+    public String handleSignIn(String nickName, String passw){
+
+        SignInConnector networkConnector = new SignInConnector();
+
+        String[] params = { nickName, passw};
+        String response;
+        try {
+            response = networkConnector.execute(params).get();
+        } catch (ExecutionException e) {
+            response = "EXECUTION_EXCEPTION";
+        } catch (InterruptedException e) {
+            response = "INTERRUPTED_EXCEPTION";
+        }
+        Log.d("RESPONSE CODE", "LOGIN RESPONSE CODE "+response);
+        return response;
+    }
+
+
+    private class SignInConnector extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... objects) {
+
+            String nick = objects[0];
+            String password = objects[1];
+
+            HttpURLConnection con = null;
+            int responseCode = 0;
+            String response="0";
+            try {
+                URL url = new URL(BASE_URL + LOGIN);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setDoOutput(true);
+                conn.setDoInput(true);
+
+                JSONObject jsonParam = new JSONObject();
+                jsonParam.put("nick", nick);
+                jsonParam.put("password", password);
+
+
+                Log.i("JSON", jsonParam.toString());
+                DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                os.writeBytes(jsonParam.toString());
+
+                os.flush();
+                os.close();
+
+                Log.i("ENDPOINT", BASE_URL + LOGIN);
+                Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                Log.i("MSG", conn.getResponseMessage());
+                response = String.valueOf(conn.getResponseCode());
+
+
+                conn.disconnect();
+            } catch (Exception e) {
+                Log.d("PIZDAAAAAAAA", "CHUJ JEBLO COS " + e);
+                e.printStackTrace();
+            }
+
+            if (responseCode == 404) {
+                return "404";
+            }
+            BufferedReader in = null;
+
+            return response;
+        }
+    }
+
+    //Maybe it should be Async
+//    public void sendPost(final String firstName, final String lastName, final String nick, final String email, final String password, final String password2, final Context ctx) {
+//        Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    URL url = new URL(BASE_URL + REGISTER);
+//                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//                    conn.setRequestMethod("POST");
+//                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+//                    conn.setRequestProperty("Accept", "application/json");
+//                    conn.setDoOutput(true);
+//                    conn.setDoInput(true);
+//
+//                    JSONObject jsonParam = new JSONObject();
+//                    jsonParam.put("first_name", firstName);
+//                    jsonParam.put("last_name", lastName);
+//                    jsonParam.put("nick", nick);
+//                    jsonParam.put("email", email);
+//                    jsonParam.put("password", password);
+//                    jsonParam.put("phone_number", "90011");
+//
+//                    Log.i("JSON", jsonParam.toString());
+//                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+//                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+//                    os.writeBytes(jsonParam.toString());
+//
+//                    os.flush();
+//                    os.close();
+//
+//                    Log.i("ENDPOINT", BASE_URL + REGISTER);
+//                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+//                    Log.i("MSG", conn.getResponseMessage());
+//                    valid = true;
+//
+//
+//                    conn.disconnect();
+//                } catch (Exception e) {
+//                    Log.d("PIZDAAAAAAAA", "CHUJ JEBLO COS " + e);
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
+//
+//        thread.start();
+//
+//    }
+
+}
