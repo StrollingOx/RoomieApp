ddb083a8d3b990f12a2056f497cdff9f07ac9540
StrollingOx <StrollingOx@github.com>
2019-11-17 14:51:21 +0100

    Small changes and fixes, i also added terrible colors!

--- app/src/main/java/com/example/rummates/MainActivity.java
+++ app/src/main/java/com/example/rummates/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import com.example.rummates.dialogs.AddCommentDialog;
 import com.example.rummates.fragments.notes.NotesFragment;
 import com.example.rummates.fragments.ShoppingListFragment;
 import com.example.rummates.fragments.ProfileFragment;
@@ -35,15 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
             switch(item.getItemId())
             {
-                case R.id.bottom_nav1:
+                case R.id.bottom_nav_shoppinglist:
                     selectedFragment = new ShoppingListFragment();
                     break;
 
-                case R.id.bottom_nav2:
+                case R.id.bottom_nav_notes:
                     selectedFragment = new NotesFragment();
                     break;
 
-                case R.id.bottom_nav3:
+                case R.id.bottom_nav_profile:
                     selectedFragment = new ProfileFragment();
                     break;
             }
